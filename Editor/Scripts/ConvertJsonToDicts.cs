cb0361c2300727225ed2e90d52178aec2baed925
Andrey <40003779+KelpieT@users.noreply.github.com>
2023-11-06 16:03:53 +0200

    Fix allocation when serialize json string

--- Editor/Scripts/ConvertJsonToDicts.cs
+++ Editor/Scripts/ConvertJsonToDicts.cs
@@ -10,24 +10,38 @@ namespace Viter.Localization.Editor
     public class ConvertJsonToDicts : UnityEditor.Editor
     {
         private string json;
+        private Object jsonTextAsset;
         public override void OnInspectorGUI()
         {
             DrawDefaultInspector();
             GUILayout.Label("Localization JSON");
-            json = EditorGUILayout.TextArea(json, GUILayout.Height(150));
-            if (!string.IsNullOrWhiteSpace(json))
+            // json = EditorGUILayout.TextArea(json, GUILayout.Height(150));
+            jsonTextAsset = EditorGUILayout.ObjectField(jsonTextAsset, typeof(TextAsset));
+            // if (!string.IsNullOrWhiteSpace(json))
+            // {
+            //     if (GUILayout.Button("Set localization from json"))
+            //     {
+            //         if (target is MainMonoLocalization mainMonoLocalization)
+            //         {
+            //             SerializableDicrionary<string, SerializableDicrionary<string, string>> allDicts = FromJson();
+            //             mainMonoLocalization.StringContainer.SetAllDicts(allDicts);
+            //         }
+            //     }
+            // }
+            if (jsonTextAsset != null)
             {
-                if (GUILayout.Button("Set localization from json"))
+                if (GUILayout.Button("Set localization from json file"))
                 {
                     if (target is MainMonoLocalization mainMonoLocalization)
                     {
+                        json = (jsonTextAsset as TextAsset).text;
                         SerializableDicrionary<string, SerializableDicrionary<string, string>> allDicts = FromJson();
                         mainMonoLocalization.StringContainer.SetAllDicts(allDicts);
                     }
                 }
             }
         }
-        
+
         public SerializableDicrionary<string, SerializableDicrionary<string, string>> FromJson()
         {
             SerializableDicrionary<string, SerializableDicrionary<string, string>> allDicts = new();
